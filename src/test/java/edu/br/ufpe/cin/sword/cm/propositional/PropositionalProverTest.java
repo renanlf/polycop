d5d2094ca8f4de12ac354210956005578298a389
Renan Fernandes <leandrorenanf@gmail.com>
2024-04-13 10:44:24 -0300

    refactoring code

--- src/test/java/edu/br/ufpe/cin/sword/cm/propositional/PropositionalProverTest.java
+++ src/test/java/edu/br/ufpe/cin/sword/cm/propositional/PropositionalProverTest.java
@@ -2,9 +2,10 @@ package edu.br.ufpe.cin.sword.cm.propositional;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
 
-import edu.br.ufpe.cin.sword.cm.propositonal.PropositionalMatrixBuilder;
 import edu.br.ufpe.cin.sword.cm.propositonal.PropositionalProver;
 import edu.br.ufpe.cin.sword.cm.tree.ExtensionProofTree;
 import edu.br.ufpe.cin.sword.cm.tree.FailProofTree;
@@ -14,53 +15,50 @@ import edu.br.ufpe.cin.sword.cm.tree.StartProofTree;
 public class PropositionalProverTest {
 
 	@Test
-	public void test() {
-		
-		PropositionalMatrixBuilder builder = new PropositionalMatrixBuilder();
-		
+	public void test() {		
 		PropositionalProver prover = new PropositionalProver();
 		
 		ProofTree<String> proof;
 		
-		proof = prover.prove(builder.matrix(
-				builder.clause("A"),
-				builder.clause("~A")
+		proof = prover.prove(List.of(
+				List.of("A"),
+				List.of("~A")
 		));
 		
 		assertTrue(proof instanceof StartProofTree<?>);
 		assertTrue(((StartProofTree<String>) proof).getChild() instanceof ExtensionProofTree<?>);
 		
-		proof = prover.prove(builder.matrix(
-				builder.clause("A"),
-				builder.clause("~B")
+		proof = prover.prove(List.of(
+				List.of("A"),
+				List.of("~B")
 		));
 		
 		assertTrue(proof instanceof FailProofTree<?>);
 		
-		proof = prover.prove(builder.matrix(
-				builder.clause("A"),
-				builder.clause("~B"),
-				builder.clause("B", "~A")
+		proof = prover.prove(List.of(
+				List.of("A"),
+				List.of("~B"),
+				List.of("B", "~A")
 		));
 		
 		assertTrue(proof instanceof StartProofTree<?>);
 		
 		System.out.println(proof.toString());
 		
-		proof = prover.prove(builder.matrix(
-				builder.clause("A"),
-				builder.clause("~B"),
-				builder.clause("B", "~A", "C"),
-				builder.clause("~C", "~B")
+		proof = prover.prove(List.of(
+				List.of("A"),
+				List.of("~B"),
+				List.of("B", "~A", "C"),
+				List.of("~C", "~B")
 		));
 		
 		assertTrue(proof instanceof FailProofTree<?>);
 		
 		System.out.println(proof.toString());
 		
-		proof = prover.prove(builder.matrix(
-				builder.clause("B", "~A"),
-				builder.clause("~B", "A")
+		proof = prover.prove(List.of(
+				List.of("B", "~A"),
+				List.of("~B", "A")
 		));
 		
 		assertTrue(proof instanceof FailProofTree<?>);
--- src/test/java/edu/br/ufpe/cin/sword/cm/prover/SimpleProverTest.java
+++ src/test/java/edu/br/ufpe/cin/sword/cm/prover/SimpleProverTest.java
@@ -1,211 +1,77 @@
 package edu.br.ufpe.cin.sword.cm.prover;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import edu.br.ufpe.cin.sword.cm.donothing.DoNothingBlockingStrategy;
-import edu.br.ufpe.cin.sword.cm.donothing.DoNothingConnectionStrategy;
-import edu.br.ufpe.cin.sword.cm.donothing.DoNothingCopyStrategy;
-import edu.br.ufpe.cin.sword.cm.donothing.DoNothingLiteralHelperStrategy;
-import edu.br.ufpe.cin.sword.cm.strategies.LiteralHelperStrategy;
-import edu.br.ufpe.cin.sword.cm.tree.AxiomProofTree;
-import edu.br.ufpe.cin.sword.cm.tree.FailProofTree;
-import edu.br.ufpe.cin.sword.cm.tree.ProofTree;
-import edu.br.ufpe.cin.sword.cm.tree.ReductionProofTree;
-import edu.br.ufpe.cin.sword.cm.tree.StartProofTree;
+import edu.br.ufpe.cin.sword.cm.tree.ProofTreeFactory;
 
 public class SimpleProverTest {
 
-    @Test
-    public void testProveWhenNoCopyIsAllowed() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.neverCopyStrategy());
-        DoNothingLiteralHelperStrategy<Integer> helperStrategy = spy(
-                DoNothingLiteralHelperStrategy.alwaysComplementaryHelperStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
+    private SimpleProver<Integer, Void, Void> prover;
 
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
+    private ProofTreeFactory<Integer> proofTreeFactory;
+
+    @Before
+    public void setUp() {
+        proofTreeFactory = spy(new ProofTreeFactory<>());
+
+        prover = new SimpleProver<Integer, Void, Void>(
+            (literal, other) -> literal == -other, 
+            (literal, other) -> literal == -other,
+            (clause) -> Optional.of(clause), 
+            (literal, clause, connState, copyState) -> false,
+            proofTreeFactory
+        );
+    }
+
+    @Test
+    public void proveWithNullMatrix_ExpectAxiom() {
+        // GIVEN
+        List<List<Integer>> matrix = null;
 
         // WHEN
-        ProofTree<Integer> proof = prover.prove(Set.of(
-                Set.of(1, 2, 3),
-                Set.of(1, 2)));
+        prover.prove(matrix);
 
         // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof FailProofTree);
-        verify(connStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).getState();
-        verify(copyStrategy, times(2)).copy(any());
-        verify(copyStrategy, times(0)).setState(any());
+        verify(proofTreeFactory, times(1)).ax(Set.of());
+        verify(proofTreeFactory, times(0)).st(any(), any(), any());
+        verify(proofTreeFactory, times(0)).red(any(), any(), any());
+        verify(proofTreeFactory, times(0)).ext(any(), any(), any(), any());
+        verify(proofTreeFactory, times(0)).fail(any(), any());
+    }
+
+    @Test
+    public void proveWithEmptyMatrix_ExpectAxiom() {
 
     }
 
     @Test
-    public void testProveWithNullMatrix() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.neverCopyStrategy());
-        DoNothingLiteralHelperStrategy<Integer> helperStrategy = spy(
-                DoNothingLiteralHelperStrategy.alwaysComplementaryHelperStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
-
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
-
-        // WHEN
-        var proof = prover.prove(null);
-
-        // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof AxiomProofTree);
-        assertEquals(Set.of(), ((AxiomProofTree<Integer>) proof).getPath());
-    }
-
-    @Test
-    public void testProveWithEmptyMatrix() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.neverCopyStrategy());
-        DoNothingLiteralHelperStrategy<Integer> helperStrategy = spy(
-                DoNothingLiteralHelperStrategy.alwaysComplementaryHelperStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
-
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
-
-        // WHEN
-        var proof = prover.prove(Set.of());
-
-        // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof AxiomProofTree);
-        assertEquals(Set.of(), ((AxiomProofTree<Integer>) proof).getPath());
-    }
-
-    @Test
-    public void testProveWithMatrixWithinEmptyClause() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.neverCopyStrategy());
-        DoNothingLiteralHelperStrategy<Integer> helperStrategy = spy(
-                DoNothingLiteralHelperStrategy.alwaysComplementaryHelperStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
-
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
-
-        // WHEN
-        var proof = prover.prove(Set.of(
-            Set.of()
-        ));
-
-        // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof FailProofTree);
-    }
-
-    @Test
-    public void testProveWithRedRule() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.alwaysCopyStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
-        LiteralHelperStrategy<Integer> helperStrategy = mock(LiteralHelperStrategy.class);
-
-        when(helperStrategy.complementaryOf(any(), any(Collection.class))).thenReturn(Set.of(1));
-
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
-
-        // WHEN
-        ProofTree<Integer> proof = prover.prove(Set.of(
-                Set.of(1),
-                Set.of(-1)));
-
-        // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof StartProofTree);
-        assertTrue(((StartProofTree<Integer>) proof).getChild() instanceof ReductionProofTree);
-        assertTrue(((ReductionProofTree<Integer>) ((StartProofTree<Integer>) proof).getChild())
-                .getChild() instanceof AxiomProofTree);
-        verify(connStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).getState();
-        verify(copyStrategy, times(1)).copy(any());
-        verify(copyStrategy, times(0)).setState(any());
-
-        verify(connStrategy, times(1)).getState();
-        verify(connStrategy, times(1)).connect(any(), any());
+    public void proveWithSingletonMatrix_ExpectFail() {
 
     }
 
     @Test
-    public void testProveWithTwoComplementaryLiteralsInThePath() {
-        // GIVEN
-        DoNothingConnectionStrategy<Integer> connStrategy = spy(DoNothingConnectionStrategy.alwaysConnectStrategy());
-        DoNothingCopyStrategy<Integer> copyStrategy = spy(DoNothingCopyStrategy.alwaysCopyStrategy());
-        DoNothingBlockingStrategy<Integer> blockStrategy = spy(DoNothingBlockingStrategy.neverBlockStrategy());
-        LiteralHelperStrategy<Integer> helperStrategy = mock(LiteralHelperStrategy.class);
-
-        when(helperStrategy.complementaryOf(any(), any(Collection.class))).thenReturn(Set.of(10, 1));
-        when(connStrategy.connect(1, 10)).thenReturn(false);
-
-        SimpleProver<Integer, Void, Void> prover = new SimpleProver<>(
-                helperStrategy,
-                connStrategy,
-                copyStrategy,
-                blockStrategy);
-
-        // WHEN
-        ProofTree<Integer> proof = prover.prove(Set.of(
-                Set.of(1),
-                Set.of(-1)));
-
-        // THEN
-        assertNotNull(proof);
-        assertTrue(proof instanceof StartProofTree);
-        assertTrue(((StartProofTree<Integer>) proof).getChild() instanceof ReductionProofTree);
-        assertTrue(((ReductionProofTree<Integer>) ((StartProofTree<Integer>) proof).getChild())
-                .getChild() instanceof AxiomProofTree);
-        verify(connStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).clear();
-        verify(copyStrategy, times(1)).getState();
-        verify(copyStrategy, times(1)).copy(any());
-        verify(copyStrategy, times(0)).setState(any());
-
-        verify(connStrategy, times(1)).getState();
-        verify(connStrategy, times(2)).connect(any(), any());
+    public void proveWithTwoComplementarySingletonClauses_ExpectExtAndAx() {
 
     }
+
+    @Test
+    public void proveWithTwoClausesButOneHasNoComplementaryLiteral_ExpectExtAndFail() {
+
+    }
+
+    @Test
+    public void proveWithComplementaryLiteralInThePath_ExpectRed() {
+
+    }
+
 }
